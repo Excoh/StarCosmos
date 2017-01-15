6d05bc4078b5711d83f254991d2180e3f2b555d1
Excel Ortega <excel.ortega@gmail.com>
2017-01-15 05:51:22 -0500

    Initial Commit

---
+++ Assets/Scripts/StarManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarManager : MonoBehaviour {
+
+    public GameObject starPrefab; // The star to spawn
+    public float maxDistance;
+    public float force = 1f;
+
+	// Use this for initialization
+	void Start () { 
+        for (int i = 0; i < 200; i++)
+        {
+            Instantiate(starPrefab, randomPos(-1, 1), Quaternion.identity);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.Space))
+        {
+            Debug.Log("Spawning Stars");
+            Instantiate(starPrefab, randomPos(-1, 1), Quaternion.identity);
+        }
+	}
+
+    Vector3 randomPos(float min, float max)
+    {
+        return new Vector3(Random.Range(min, max), Random.Range(min, max), Random.Range(min, max));
+    }
+}
---
+++ Assets/Scripts/starscript.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+using VRStandardAssets.Utils;
+
+public class starscript : MonoBehaviour {
+
+    public VRInteractiveItem intItem;
+    public Rigidbody rb;
+    public float force;
+    public float maxLength;
+    public GameObject starManager;
+    public bool wasHit;
+    // Use this for initialization
+    void Start ()
+    {
+        rb = GetComponent <Rigidbody>();
+        starManager = GameObject.FindWithTag("StarManager");
+        maxLength = starManager.GetComponent<StarManager>().maxDistance;
+        force = starManager.GetComponent<StarManager>().force;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        NormalMove();
+        UpdateMove();
+    }
+
+    void OnEnable()
+    {
+        intItem.OnClick += StopStar;
+    }
+
+    void OnDisable()
+    {
+        intItem.OnClick -= StopStar;
+    }
+    
+    void NormalMove() // The normal movement of the star, centered around the origin
+    {
+        // if went past a certain amount, we stop adding force
+        if (!wasHit)
+        {
+            if ((transform.position - Vector3.zero).magnitude < maxLength)
+            {
+                rb.AddForce((transform.forward * 1 / (transform.position - Vector3.zero).magnitude * force));
+            }
+            transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+        }
+    }
+
+    void UpdateMove()
+    {
+        if (Input.GetKey(KeyCode.J)) // pull in and slow down the stars
+        {
+            rb.AddForce(transform.forward * -force * 2);
+        }
+
+        if (Input.GetKey(KeyCode.L)) // push out and speed up the stars 
+        {
+            rb.AddForce(transform.forward * force * 2);
+        }
+    }
+
+    // star gets hit
+    // star stops, turns white
+    public void StopStar()
+    {
+        print("The star has stopped.");
+        rb.velocity = Vector3.zero;
+        rb.angularVelocity = Vector3.zero;
+        wasHit = true;
+    }
+}
